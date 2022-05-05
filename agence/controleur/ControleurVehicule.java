d503cc292ee1764bc3107a2ef49bf17c84115085
Gansonre Ismael <proismaelgans2017@gmail.com>
2022-05-05 06:34:09 -0400

    tester

---
+++ agence/controleur/ControleurVehicule.java
@@ -0,0 +1,33 @@
+package controleur;
+
+import modele.Vehicule;
+import stockage.StockagePersistant;
+
+import java.util.List;
+
+public class ControleurVehicule {
+    private Vehicule vehicule;
+    private List<Vehicule> listeVehicule;
+    private StockagePersistant stockage;
+
+    public Vehicule getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(Vehicule vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public List<Vehicule> getListeVehicule() {
+        return listeVehicule;
+    }
+
+    public void setListeVehicule(List<Vehicule> listeVehicule) {
+        this.listeVehicule = listeVehicule;
+    }
+
+    public void sauvegarde (Vehicule vehicule){
+        stockage.sauvegardeVehicule(vehicule);
+    }
+
+}
